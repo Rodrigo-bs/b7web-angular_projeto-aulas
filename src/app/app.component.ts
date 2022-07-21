236a587ffa9931fdd64e78aa1a75e8d969591bd0
unknown <rodrigo.bs.548@gmail.com>
2022-07-21 19:31:39 -0300

    Laços de repetição com angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,15 @@ export class AppComponent {
   title = 'angular-aulas';
   nomes = ['Rodrigo', 'Alisson', 'Pedro', 'Ronaldo'];
   meuNome = this.nomes[0];
+
+  linguagens = ['html', 'css', 'js', 'php'];
+
+  pessoas = [
+    {nome: 'Rodrigo', idade: 19},
+    {nome: 'Alisson', idade: 20},
+    {nome: 'Pedro', idade: 22},
+    {nome: 'Ronaldo', idade: 31}
+  ];
   
   soma() {
     return 'Essa é a função soma.';
